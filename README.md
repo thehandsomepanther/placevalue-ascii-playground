949fabb22cf9861bafea7365c1dc8296a5c98598
Josh Shi <thehandsomepanther@gmail.com>
2019-03-05 22:33:47 -0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Placevalue Ascii Playground
-
-Small demo app to play around with [Mouse Reeve's Placevalue Ascii](https://github.com/mouse-reeve/placevalue_ascii).
-
-I've implemented pretty much everything I wanted to implement, so this project probably won't see any major updates unless there's a compelling reason for it.
---
+++ precache-manifest.df62a412c0463b8efdfae397362b116b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa36c23d4d06c6f95b37",
+    "url": "/placevalue-ascii-playground/static/js/runtime~main.fa36c23d.js"
+  },
+  {
+    "revision": "16fd0d483bb415dd7612",
+    "url": "/placevalue-ascii-playground/static/js/main.16fd0d48.chunk.js"
+  },
+  {
+    "revision": "07fef3e72da268b8a485",
+    "url": "/placevalue-ascii-playground/static/js/2.07fef3e7.chunk.js"
+  },
+  {
+    "revision": "16fd0d483bb415dd7612",
+    "url": "/placevalue-ascii-playground/static/css/main.22eb0756.chunk.css"
+  },
+  {
+    "revision": "868e18d0544da07a2f97751cb60e5e21",
+    "url": "/placevalue-ascii-playground/index.html"
+  }
+];
